3c7abd9c31e1ed56450b11bd07bc034abdee4af2
grindsa <grindelsack@gmail.com>
2018-02-08 08:56:56 +0100

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name='dkb_robo',
-      version='0.4',
-      description='library to access the internet banking area of "Deutsche Kreditbank" to get account information and transactions.',
-      url='https://github.com/grindsa/dkb-robo',
-      author='grindsa',
-      author_email='grindelsack@gmail.com',
-      license='GPL',
-      packages=['dkb_robo'],
-      zip_safe=False)
+    version='0.4',
+    description='library to access the internet banking area of "Deutsche Kreditbank" to get account information and transactions.',
+    url='https://github.com/grindsa/dkb-robo',
+    author='grindsa',
+    author_email='grindelsack@gmail.com',
+    license='GPL',
+    packages=['dkb_robo'],
+    install_requires=[
+        'mechanize',
+        'bs4',
+        'html5lib',
+        'six'
+    ],
+    zip_safe=False)
