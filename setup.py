919ee9d401c0bedd82c973d8c2f60a8661629fd7
grindsa <grindelsack@gmail.com>
2020-03-29 08:40:01 +0200

    [feat] bump to 0.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dkb_robo',
-    version='0.8.3',
+    version='0.9',
     description='library to access the internet banking area of "Deutsche Kreditbank" to get account information and transactions.',
     url='https://github.com/grindsa/dkb-robo',
     author='grindsa',
